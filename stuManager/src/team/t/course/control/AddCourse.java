5e1bee754444d6a4990fcc06afd22e2f7243383c
LiamHua <1120601455@qq.com>
2019-12-17 14:26:44 +0800

    2019.12.17

--- stuManager/src/team/t/course/control/AddCourse.java
+++
@@ -1,4 +0,0 @@
-package team.t.course.control;
-
-public class AddCourse {
-}
--- stuManager/src/team/t/course/web/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "pers.liam.countManager.MyService")
-package team.t.course.web;
